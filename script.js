68cb38d590e0843d61615174507b9d7070626766
Chaigood <cj.haigood@gmail.com>
2020-11-10 20:18:25 -0600

    JS issues

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ weather.temperature = {
 //const and vars
 const KELVIN = 273;
 // api stuff
-const key = "82005d27a116c2880c8f0fcb866998a0";
-//Geo stuff
-if("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
+// const key = "35252ba040caf2313135e33b5e8aca45";
+const key = "382005d27a116c2880c8f0fcb866998a0";
+//Geo Stuff
+if('geolocation' in navigator){
+    // navigator.geolocation.getCurrentPosition(setPosition, showError);
+    navigator.geolocation.watchPosition(setPosition, showError);
 }else{
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Browser doesn't support location</p>";
@@ -25,18 +27,44 @@ if("geolocation" in navigator){
 
 //set user position
 function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+     let latitude = position.coords.latitude;
+     let longitude = position.coords.longitude;
+    //let CityName = positioni.coords.lati
 
     getWeather(latitude, longitude);
+    // getWeather(CityName);
 }
 
 //error
 function showError(error){
     notificationElement.style.display = "block";
-    notificationElement.innerHTML = <p> ${error.message} </p> ;
+    notificationElement.innerHTML = "<p> ${error.message} </p>";
 }
 // get weather from api
-function getWeather(latitude, longitude){
-    let App
+ function getWeather(latitude, longitude){
+    //function getWeather(CityName){ 
+    let api = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}';
+
+    // console.log(api);
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].icon;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+function displayWeather(){
+    iconElement.innerHTML = '<img src="icons/${weather.iconId}.png"/>';
+    tempElement.innerHTML = '${weather.temperature.value}°<span>C</span>';
+    descElement.innerHTML = weather.description;
+    loactionElement.innerHTML = '${weather.city}, ${weather.country}';
 }
